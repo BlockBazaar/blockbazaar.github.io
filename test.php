78924f01990f1b54d4f91b99d0cd4ae89b99c1eb
Block Bazaar <98306204+BlockBazaar@users.noreply.github.com>
2022-04-07 12:41:33 +0530

    Update test.php

--- test.php
+++ test.php
@@ -4,18 +4,16 @@
 </body>
 </html>
 <?php
-$host = "ec2-52-201-124-168.compute-1.amazonaws.com";
-$user = "pcfuiaofaalxez";
-$pass = "8cf9be157e275bd1ac5c0e41155c79a2ca501f30a9345bdf35cc1a79709a83bb";
-$db = "d30urd6dg1iirk";
-$con = pg_connect ("host=$host dbname=$db user=$user password=$pass") or die ("Could not connect to server")
-if ($con) {
-	echo "Error : Unable to open database\n";
-} else {
-    $name = 'avishikta';
-    $password = 'avka'
-	$query = "INSERT INTO user2(name,password) VALUES ($name,$password)";
-	$result = pg_query($con, $query);
+$servername = "remotemysql.com"
+$username = "gS467AnmIZ";
+$password = "mXgN6pphV7";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
-pg_close($con);
+echo "Connected successfully";
 ?>
