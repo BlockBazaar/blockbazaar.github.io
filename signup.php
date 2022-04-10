7e099d149ebf9ab78bf134ad4ed3847dbee9ef8f
Block Bazaar <98306204+BlockBazaar@users.noreply.github.com>
2022-04-10 13:22:06 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -54,11 +54,11 @@
 		}
 	</style>
 	<script>
-
 		function bodyLoad() {
 			captcha();
 			dateRange();
 		}
+
 		function captcha() {
 			var randomString = "";
 			var characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz!@#$%&()+=-<>/?[]{}";
@@ -77,13 +77,13 @@
 			var yyyy = dt.getFullYear() - 13;
 
 			if (dd < 10) {
-			dd = '0' + dd;
+				dd = '0' + dd;
 			}
 
 			if (mm < 10) {
-			mm = '0' + mm;
-			} 
-				
+				mm = '0' + mm;
+			}
+
 			dt = yyyy + '-' + mm + '-' + dd;
 			document.getElementById("dob").setAttribute("max", dt);
 		}
@@ -107,8 +107,7 @@
 				var msg = "Are you sure you want to submit this form and all provided details are correct?";
 				if (confirm(msg) === true) {
 					return true;
-				}
-				else {
+				} else {
 					return false;
 				}
 			}
@@ -135,22 +134,21 @@
 									<label class="form-label" for="fullName">
 										<font style="color: red;">*</font>Full Name
 									</label>
-									<input type="text" class="form-control text-center" id="fullName" required>
+									<input type="text" class="form-control text-center" id="fullName" name="fullName" maxlength="100" required>
 								</div>
 								<div class="col-sm-5">
 									<label class="form-label" for="email">
 										<font style="color: red;">*</font>Email
 									</label>
-									<input type="email" class="form-control text-center" id="email" required>
+									<input type="email" class="form-control text-center" id="email" name='email' required>
 								</div>
 							</div>
 							<div class="row gx-3 gy-2 mt-2 mb-2 justify-content-around align-items-end">
 								<div class="col-sm-5">
-									<label class="form-label" for="Contact">
+									<label class="form-label" for="contact">
 										<font style="color: red;">*</font>Contact Number
 									</label>
-									<input type="tel" pattern="[0-9]{10}" class="form-control text-center" id="contact"
-										required>
+									<input type="tel" pattern="[0-9]{10}" class="form-control text-center" id="contact" name='contact' required>
 								</div>
 								<div class="col-sm-5">
 									<label class="form-label" for="dob">
@@ -164,10 +162,8 @@
 									<label class="form-label" for="pswrd">
 										<font style="color: red;">*</font>Password
 									</label>
-									<input type="password" class="form-control text-center" id="pswrd" name="pswd"
-										pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required> <br>
-									<input class="form-check-input" type="checkbox" value="" id="showPswrd"
-										name="showPswd" onclick="showP()">
+									<input type="password" class="form-control text-center" id="pswrd" name="pswd" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" maxlength="100" required> <br>
+									<input class="form-check-input" type="checkbox" value="" id="showPswrd" name="showPswd" onclick="showP()">
 									<label class="form-check-label" for="showPswrd">
 										Show Password
 									</label>
@@ -178,32 +174,14 @@
 									Password must be of atleast 8 characters and must have atleast <br> 1 uppercase
 									character, 1 lowercase character, 1 digit and 1 special character.
 								</div>
-							</div>
-							<div class="row gx-3 gy-2 mt-2 mb-2 justify-content-around align-items-end">
-								<div class="col-sm-5">
-									<label class="form-label" for="address">Address</label>
-									<input type="text" class="form-control text-center" id="address" name="address">
-								</div>
-								<div class="col-sm-5">
-									<label class="form-label" for="zipCode">Zip Code</label>
-									<input type="text" pattern="[0-9].{}" class="form-control text-center" id="zipCode"
-										name="zipCode">
-								</div>
-							</div>
+							</div>							
 							<div class="row gx-3 gy-2 mt-2 mb-2 justify-content-around align-items-end">
 								<div class="col-sm-5">
 									<label class="form-label" for="country">
 										<font style="color: red;">*</font>Country
 									</label>
-									<input type="text" class="form-control text-center" id="country" name="country"
-										required>
+									<input type="text" class="form-control text-center" id="country" name="country" maxlength="100" required>
 								</div>
-								<div class="col-sm-5">
-									<label class="form-label" for="state">State</label>
-									<input type="text" class="form-control text-center" id="state" name="state">
-								</div>
-							</div>
-							<div class="row gx-3 gy-2 mt-2 mb-2 justify-content-around align-items-end">
 								<div class="col-sm-5">
 									<label class="form-label" for="currency">
 										<font style="color: red;">*</font>Currency
@@ -213,48 +191,39 @@
 										<option value="EUR">EUR</option>
 										<option value="INR">INR</option>
 									</select>
-								</div>
+								</div>								
 							</div>
+							
 							<div class="row gx-3 gy-2 mt-2 justify-content-around align-items-end">
 								<div class="col-sm-4">
 									<label class="form-label" for="mainCaptcha">CAPTCHA</label>
-									<input type="text" id="mainCaptcha" name="mainCaptcha"
-										class="form-control text-center align-items-center" disabled />
+									<input type="text" id="mainCaptcha" name="mainCaptcha" class="form-control text-center align-items-center" disabled />
 								</div>
 								<div class="col-sm-2">
 									<br>
-									<button type="button" name="refresh" class="btn btn-light align-content-center"
-										value="Refresh" onclick="captcha();">
-										<svg width="20" height="20" class="bi bi-arrow-counterclockwise"
-											viewBox="0 0 16 16">
-											<path fill-rule="evenodd"
-												d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z" />
-											<path
-												d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z" />
+									<button type="button" name="refresh" class="btn btn-light align-content-center" value="Refresh" onclick="captcha();">
+										<svg width="20" height="20" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
+											<path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z" />
+											<path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z" />
 										</svg>
 									</button>
 								</div>
 								<div class="col-sm-4 mt-2">
-									<div id="captchaHelpBlock"
-										class="form-text align-content-center justify-content-around">
+									<div id="captchaHelpBlock" class="form-text align-content-center justify-content-around">
 										Type the given characters <br>
 									</div>
-									<input type="text" class="form-control text-center justify-content-around"
-										id="userCaptcha" name="userCaptcha">
+									<input type="text" class="form-control text-center justify-content-around" id="userCaptcha" name="userCaptcha">
 								</div>
 							</div>
 							<div class="row gx-3 gy-2 mt-4 mb-2 align-items-center justify-content-around">
 								<div class="col-sm-4">
-									<a class="btn btn-primary me-md-2" target="_self" href="index.php"
-										role="button">Back</a>
+									<a class="btn btn-primary me-md-2" target="_self" href="index.php" role="button">Back</a>
 								</div>
 								<div class="col-sm-2">
-									<button type="reset" class="btn btn-primary me-md-2" value="reset"
-										onclick="captcha()">Reset</button>
+									<button type="reset" class="btn btn-primary me-md-2" value="reset" onclick="captcha()">Reset</button>
 								</div>
 								<div class="col-sm-4">
-									<button type="submit" class="btn btn-danger btn-primary me-md-2"
-										data-bs-dismiss="modal" value="submit">Sign Up</button>
+									<button type="submit" class="btn btn-danger btn-primary me-md-2" data-bs-dismiss="modal" value="submit">Sign Up</button>
 								</div>
 							</div>
 						</form>
@@ -266,29 +235,3 @@
 </body>
 
 </html>
-<?php
-$host = "ec2-52-201-124-168.compute-1.amazonaws.com";
-$user = "pcfuiaofaalxez";
-$pass = "8cf9be157e275bd1ac5c0e41155c79a2ca501f30a9345bdf35cc1a79709a83bb";
-$db = "d30urd6dg1iirk";
-$con = pg_connect ("host=$host dbname=$db user=$user password=$pass") or die ("Could not connect to server")
-if ($con) {
-	echo "Error : Unable to open database\n";
-} else {
-	$name = $_POST['Full Name'];
-	$mail = $_POST['Email'];
-	$con_num = $_POST['Contact Number'];
-	$date = $_POST['Date of Birth'];
-	$password = $_POST['Password'];
-	$address = $_POST['Address'];
-	$zipcode = $_POST['Zip Code'];
-	$country = $_POST['Country'];
-	$state = $_POST['State'];
-	$currency = $_POST['Currency'];
-
-	$query = "INSERT INTO usercredentials(Full Name,Email,Contact Number,Date of Birth, Password,Address, Zip Code,Country, State, Currency) VALUES ($name,$mail,$con_num,$date,$password,$address,$zipcode,$country,$state,$currency)";
-	$result = pg_query($con, $query);
-	header("location : signUpResponse.php");
-}
-pg_close($con);
-?>
